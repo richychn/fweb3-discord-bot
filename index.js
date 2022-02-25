5d223757c3778d64c3b6e43d1ff612cd861f8742
Richy Chen <richy@ip-192-168-1-151.ec2.internal>
2022-02-24 22:42:55 -0500

    newer block

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ const web3 = new Web3(provider);
 const contract = new web3.eth.Contract(abi, contractAddress);
 
 const options = {
-    fromBlock: 25309969
+    fromBlock: 25310520
 }
 
 client.on('messageCreate', async msg => {
