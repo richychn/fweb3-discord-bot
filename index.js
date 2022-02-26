6f8602ffc90818b8e52907794573c44e2e65acc3
Richy Chen <richy@richys-macbook.myfiosgateway.com>
2022-02-26 10:39:43 -0500

    changed to webhook

--- index.js
+++ index.js
@@ -1,86 +1,37 @@
 require('dotenv').config(); 
 const axios = require('axios');
 const Web3 = require('web3');
-const Discord = require('discord.js'); 
+const { send } = require("@ayshptk/msngr");
 
 const contractAddress = '0xc6c5F7B1a27528DD6F34EF164377965114bfA7D9';
 const abi = [{"inputs":[{"internalType":"contract IERC20","name":"token","type":"address"}],"stateMutability":"nonpayable","type":"constructor"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"previousOwner","type":"address"},{"indexed":true,"internalType":"address","name":"newOwner","type":"address"}],"name":"OwnershipTransferred","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"_player","type":"address"}],"name":"PlayerSeeksVerification","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"_player","type":"address"},{"indexed":true,"internalType":"address","name":"_judge","type":"address"}],"name":"PlayerVerifiedToWin","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"player","type":"address"}],"name":"PlayerWon","type":"event"},{"inputs":[{"internalType":"address","name":"judge","type":"address"}],"name":"addJudge","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[],"name":"getJudges","outputs":[{"internalType":"address[]","name":"","type":"address[]"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"player","type":"address"}],"name":"hasBeenVerifiedToWin","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"player","type":"address"}],"name":"hasNotWonBefore","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"player","type":"address"}],"name":"hasTokens","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"judge","type":"address"}],"name":"isJudge","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"player","type":"address"}],"name":"isWinner","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"owner","outputs":[{"internalType":"address","name":"","type":"address"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"judge","type":"address"}],"name":"removeJudge","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[],"name":"renounceOwnership","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[],"name":"seekVerification","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"newOwner","type":"address"}],"name":"transferOwnership","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"player","type":"address"}],"name":"verifyPlayer","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[],"name":"win","outputs":[],"stateMutability":"nonpayable","type":"function"}];
-
-const client = new Discord.Client({intents: ['GUILDS', 'GUILD_MESSAGES']});
-// log in the bot
-client.on('ready', () => {
-    console.log(`Logged in as ${client.user.tag}!`);
-});
 
 // set up connection to Polygon
 const provider = new Web3.providers.WebsocketProvider(process.env.NODE_URL);
 const web3 = new Web3(provider);
 const contract = new web3.eth.Contract(abi, contractAddress);
 const fweb3_api = 'https://fweb3.xyz/api/polygon?wallet_address=';
-const dots = ['hasUsedFaucet', 'hasSentTokens', 'hasMintedNFT', 'hasBurnedTokens', 'hasSwappedTokens', 'hasVotedInPoll', 'hasDeployedContract']
+const webhook = process.env.WEBHOOK_URL;
 
-const options = {
-    fromBlock: 25337425
-}
-
-client.on('messageCreate', async msg => {
-    switch (msg.content) {
-        case "!connect-seek-verify":
-            msg.channel.send("You are now subscribed to notifications of verification requests.");
-            contract.events.PlayerSeeksVerification(options)
-                .on('data', event => {
-                    console.log(event);
-                    axios.get(fweb3_api + event['returnValues']['_player']).then(function(res) {
-                        let won = true;
-                        for (const [key, value] of Object.entries(res.data)) {
-                            if (key === "tokenBalance") {
-                                won = value >= 100;
-                            } else {
-                                won = value;
-                            }
-                            if (!won) {
-                                console.log(res.data)
-                                msg.channel.send("Player is seeking verification, but has NOT completed: https://fweb3.xyz/?wallet=" + event['returnValues']['_player'] + '\n');
-                                break;
-                            }
-                        }
-                        if (won) {
-                            msg.channel.send("Player is seeking verification, and has completed: https://fweb3.xyz/?wallet=" + event['returnValues']['_player'] + '\n');
-                        }
-                    })
-                })
-                .on('changed', changed => console.log(changed))
-                .on('error', err => console.log(err))
-                .on('connected', str => console.log(str))
-            break;
-        case "!connect-verified":
-            msg.channel.send("You are now subscribed to notifications of successful verifications.");
-
-            contract.events.PlayerVerifiedToWin(options)
-                .on('data', event => {
-                    console.log(event);
-                    msg.channel.send("Player " + event['returnValues']['_player'] + " has been verified by " + event['returnValues']['_judge']);
-                })
-                .on('changed', changed => console.log(changed))
-                .on('error', err => console.log(err))
-                .on('connected', str => console.log(str))
-            break;
-        case "!connect-winners":
-            msg.channel.send("You are now subscribed to notifications of new winners.");
-
-            contract.events.PlayerWon(options)
-                .on('data', event => {
-                    console.log(event);
-                    msg.channel.send("Player " + event['returnValues']['_player'] + " has won!!!");
-                })
-                .on('changed', changed => console.log(changed))
-                .on('error', err => console.log(err))
-                .on('connected', str => console.log(str))
-            break;
-    }
+contract.events.PlayerSeeksVerification().on('data', async event => {
+    console.log(event);
+    axios.get(fweb3_api + event['returnValues']['_player']).then(async function(res) {
+        let won = true;
+        for (const [key, value] of Object.entries(res.data)) {
+            if (key === "tokenBalance") {
+                won = value >= 100;
+            } else {
+                won = value;
+            }
+            if (!won) {
+                console.log(res.data)
+                await send(webhook, "Player is seeking verification, but has NOT completed: https://fweb3.xyz/?wallet=" + event['returnValues']['_player'] + '\n');
+                break;
+            }
+        }
+        if (won) {
+            await send(webhook, "Player is seeking verification, and has completed: https://fweb3.xyz/?wallet=" + event['returnValues']['_player'] + '\n');
+        }
+    })
 })
-
-
-//make sure this line is the last line
-client.login(process.env.DISCORD_CLIENT_TOKEN); 
 
